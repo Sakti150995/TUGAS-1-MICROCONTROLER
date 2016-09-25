b500236808884edcddf98ddbe7dcc6ae502caeaa
Sakti150995 <scrmha@gmail.com>
2016-09-26 00:40:52 +0700

    SINTAX IN ARDUINO
    UNTUK NAMA DITULIS DIDALAM PROGRAM DENGAN HIDDEN

---
+++ praktikum_1/praktikum_1.ino
@@ -0,0 +1,13 @@
+//PRATIKUM 1 SAKTI ABDULLAH(4211511015)//
+int led=13;
+void setup() {
+ pinMode(led,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+  
+}
